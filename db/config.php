d66fdb8ce030d66ea2bfa69cafc0dfd2332366a1
danielsuter <suter_daniel@hotmail.com>
2014-10-05 16:58:11 -0700

    Initial commit

---
+++ db/config.php
@@ -0,0 +1,26 @@
+<?php
+$config = array();
+$config['db'] = 'mysql';
+$config['mysql.host'] = 'localhost';
+$config['mysql.db'] = 'mytinytodo';
+$config['mysql.user'] = 'root';
+$config['mysql.password'] = '';
+$config['prefix'] = 'mtt_';
+$config['url'] = '';
+$config['mtt_url'] = '';
+$config['title'] = '';
+$config['lang'] = 'en';
+$config['password'] = '';
+$config['smartsyntax'] = 1;
+$config['timezone'] = 'UTC';
+$config['autotag'] = 1;
+$config['duedateformat'] = 1;
+$config['firstdayofweek'] = 1;
+$config['session'] = 'files';
+$config['clock'] = 24;
+$config['dateformat'] = 'j M Y';
+$config['dateformat2'] = 'n/j/y';
+$config['dateformatshort'] = 'j M';
+$config['template'] = 'default';
+$config['showdate'] = 0;
+?>
